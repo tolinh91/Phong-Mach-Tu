6e3a603f3dbb0e7670e7f2fee8bc34b39399bffe
tolinh91 <tolinh.tran91@gmail.com>
2025-07-05 22:21:07 +0900

    dantrang1

--- frontend/phongmachtu/src/App.jsx
+++ frontend/phongmachtu/src/App.jsx
@@ -3,13 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import logo1 from './assets/logo1.jpg'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import NavigationMenu from './components/NavigationMenu';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+import Content from './components/Content';
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className="left" style={{ width: '40%', float: 'left', backgroundColor: "#cceeff"
+    <>    
+    <Header />
+    <NavigationMenu />
+    <Content />
+    <MainContent/>
+    <Content />
+    <Footer />
+
+   
+
+    {/* <div className="left" style={{ width: '40%', float: 'left', backgroundColor: "#cceeff"
      }}>
       <img src = {logo1} alt=""/>
 
@@ -18,9 +36,9 @@ function App() {
       <div className="right" style={{ width: '60%', float: 'right', backgroundColor: 'blue' }}>
         <p>This is a paragraph.</p>
         <p>This is a paragraph.2</p>
-      </div>
-    </>
+      </div> */}
+  </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/phongmachtu/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <>
+        This is Header 
+        <p>Header is here</p>
+      
+      </>
+
+
+
+    )
+
+}
+export default Header;
